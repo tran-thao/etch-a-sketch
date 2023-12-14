7d76b16ecb091f594c2b98190c01ba099cb9216d
“thaotran“ <phtranthao@gmail.com>
2023-12-14 17:24:38 +0100

    Add flexible grid size through user input

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
 const container = document.querySelector('.container');
 
-//Create 16x16 grid of divs
-for(let i = 0; i < 16*16; i++){
-    const square = document.createElement('div');
-    square.classList.add("square");
-    square.addEventListener('mouseover', function() {
-        this.style.backgroundColor = 'black';
-    });
-    container.appendChild(square);
+function createGrid (size) {
+    removeAllChildNodes(container);
+    for(let i = 0; i < size*size; i++){
+        const square = document.createElement('div');
+        square.classList.add("square");
+        square.addEventListener('mouseover', function() {
+            this.style.backgroundColor = 'black';
+        });
+        const squareWidth = (container.offsetWidth-2)/size;
+        square.style.flexBasis = `${squareWidth-2}px`;
+        container.appendChild(square);
+    }
+}
+
+const button = document.querySelector('.button');
+button.addEventListener('click', function(){
+        createGrid(getSize());
+});
+
+const getSize = () => {
+    let newSize;
+    do {
+        newSize = window.prompt("Enter a number from 1-100", 3);
+    } while (newSize > 100 || newSize <= 0);
+    return newSize;
+}
+
+function removeAllChildNodes(element){
+    while(element.firstChild){
+        element.removeChild(element.firstChild);
+    }
 }
